c9dc1ce2c829ef9837ddffe89601b181adf80e8e
MatchaMiG <112700164+MatchaMiG@users.noreply.github.com>
2023-10-04 00:47:41 +0900

    設定ファイルを更新

--- bp_raid_notify/__main__.py
+++ bp_raid_notify/__main__.py
@@ -1,7 +1,6 @@
 """!@package bp_raid_notifyメインモジュール
 @note created by https://twitter.com/MatchaMiG
-@date 2023.09.27
+@date 2023.10.03
 """
 if __name__== '__main__':
-    from . import setenv        # 環境変数設定 ※gitignore対象
-    from . import time_signal   # 起動
+    from . import bp_raid_notify    # 起動
